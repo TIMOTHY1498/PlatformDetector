05ab8b144fdc9fa5e7ab9eb4eca31ef6f2bbc8b4
Timothy B <74752830+TIMOTHY1498@users.noreply.github.com>
2025-02-01 21:11:09 +0700

    Add files via upload

---
+++ PlatformDetector.lua
@@ -0,0 +1,51 @@
+--[[
+	// PlatformDetector.lua - An extenstion that used for checking the device platform.
+	
+	// Created by PakTimoGus124
+	// Version 0.1
+]]--
+
+local MOBILE = game.UserInputService.TouchEnabled
+local CONSOLE = game.UserInputService.GamepadEnabled
+local PC = game.UserInputService.KeyboardEnabled and game.UserInputService.MouseEnabled
+
+function getViewportSize()
+	while not game.Workspace.CurrentCamera do
+		game.Workspace.Changed:wait()
+	end
+
+	-- ViewportSize is initally set to 1, 1 in Camera.cpp constructor.
+	-- Also check against 0, 0 incase this is changed in the future.
+	while game.Workspace.CurrentCamera.ViewportSize == Vector2.new(0,0) or
+		game.Workspace.CurrentCamera.ViewportSize == Vector2.new(1,1) do
+		game.Workspace.CurrentCamera.Changed:wait()
+	end
+
+	return game.Workspace.CurrentCamera.ViewportSize
+end
+
+--// Public APIs
+
+local apis = {}
+
+function apis:Check()
+	if MOBILE then
+		if MOBILE and (getViewportSize().Y <= 370) then
+			return "Phone"
+		else
+			return "Tablet"
+		end
+	elseif CONSOLE then
+		if CONSOLE then
+			return Enum.Platform.XBoxOne
+		elseif CONSOLE and (getViewportSize().Y <= 1440) then
+			return Enum.Platform.PS5
+		end
+	elseif PC then
+		return Enum.Platform.Windows
+	else
+		return Enum.Platform.None
+	end
+end
+
+return apis
